a67f142bbe969f982766a336856c301262da9329
Mimi_G <41672186+Greenokorohmichelle@users.noreply.github.com>
2021-04-25 11:59:16 +0100

    Update atm.py

--- atm.py
+++ atm.py
@@ -1,81 +1,93 @@
-
-from datetime import datetime
-print(datetime.now())
-username = input("Enter a username: \n")
-database_username = {
-   'Seyi':'passwordSeyi',
-   'Mike':'passwordMike',
-   'Love':'passwordLove'
-}
-
-def login():
-    #login function here
-    name = input("What is your name? \n")
-    password = input("Your password? \n")
-    if(name in database_user and password == database_user[name]):
-        print("Welcome " + name)
-        return True
-    else:
-        print("Password or Username Incorrect. Please try again")
-        return False
-import re
-def validate():
-    while True:
-        password = input("Enter a password: \n")
-        if len(password) < 8:
-            print("Make sure your password is at lest 8 letters")
-        elif re.search('[0-9]',password) is None:
-            print("Make sure your password has a number in it")
-        elif re.search('[A-Z]',password) is None: 
-            print("Make sure your password has a capital letter in it")
-        else:
-            print("Your password seems fine")
-            break
-validate()
-
-print("Welcome %s" % username )
-print('These are the available options:')
-print('1.Withdrawal')
-print('2.Cash Deposit')
-print('3.Complaint')
-
-SelectedOption = int(input('Please select an option: \n'))
-if SelectedOption is 1 :
-    input('How much would you like to withdraw: \n')
-    print('Take your cash')
-    options = input("Do you want to go back to the available options ? Enter Y for yes or N for no \n")
-if options.upper() == 'Y':
-    print('These are the available options:')
-    print('1.Withdrawal')
-    print('2.Cash Deposit')
-    print('3.Complaint')
-    SelectedOption = int(input('Please select an option: \n'))
-    
-else:
-    print('Thank you for using the ATM machine')
-
-if SelectedOption is 2 :
-    balance = input('How much would you like to deposit: \n') 
-    print('Current Balance: %s' %balance)
-    options = input("Do you want to go back to the available options ? Enter Y for yes or N for no \n")
-if options.upper() == 'Y':
-    print('These are the available options:')
-    print('1.Withdrawal')
-    print('2.Cash Deposit')
-    print('3.Complaint')
-    SelectedOption = int(input('Please select an option: \n'))
-else:
-    print('Thank you for using the ATM machine')
-    
-if SelectedOption is 3 :
-    input('What issue would you like to report: \n')
-    print('Thank you for contacting us')
-    options = input("Do you want to go back to the available options ? Enter Y for yes or N for no \n")
-if options.upper() == 'Y':
-    print('These are the available options:')
-    print('1.Withdrawal')
-    print('2.Cash Deposit')
-    print('3.Complaint')
-    SelectedOption = int(input('Please select an option: \n'))
-else:
-    print('Thank you for using the ATM machine')
+import random
+ 
+class Account:
+      def __init__(self, id, balance = 0, annualInterestRate = 3.4):
+        self.id = id
+        self.balance = balance
+        self.annualInterestRate = annualInterestRate
+ 
+    def getId(self):
+        return self.id
+ 
+    def getBalance(self):
+        return self.balance
+ 
+    def getAnnualInterestRate(self):
+        return self.annualInterestRate
+ 
+    def getMonthlyInterestRate(self):
+        return self.annualInterestRate / 12
+ 
+    def withdraw(self, amount):
+        self.balance -= amount
+ 
+    def deposit(self, amount):
+        self.balance += amount
+ 
+    def getMonthlyInterest(self):
+        return self.balance * self.getMonthlyInterestRate()
+      
+     def main():
+     accounts = []
+   for i in range(1000, 9999):
+       account = Account(i, 0)
+       accounts.append(account)
+     while True:
+ 
+         id = int(input("\nEnter account pin: "))
+ 
+         while id < 1000 or id > 9999:
+           id = int(input("\nInvalid Id.. Re-enter: "))
+ 
+         while True:
+ 
+           print("\n1 - View Balance \t 2 - Withdraw \t 3 - Deposit \t 4 - Exit ")
+ 
+            selection = int(input("\nEnter your selection: "))
+ 
+            for acc in accounts:
+               if acc.getId() == id:
+                   accountObj = acc
+                   break
+ 
+           if selection == 1:
+               print(accountObj.getBalance())
+ 
+           elif selection == 2:
+               amt = float(input("\nEnter amount to withdraw: "))
+               ver_withdraw = input("Is this the correct amount, Yes or No ? " + str(amt) + " ")
+ 
+               if ver_withdraw == "Yes":
+                   print("Verify withdraw")
+               else:
+                   break
+ 
+               if amt < accountObj.getBalance():
+                  accountObj.withdraw(amt)
+                  print("\nUpdated Balance: " + str(accountObj.getBalance()) + " n")
+               else:
+                    print("\nYou're balance is less than withdrawl amount: " + str(accountObj.getBalance()) + " n")
+                    print("\nPlease make a deposit.");
+ 
+           elif selection == 3:
+               amt = float(input("\nEnter amount to deposit: "))
+               ver_deposit = input("Is this the correct amount, Yes, or No ? " + str(amt) + " ")
+ 
+               if ver_deposit == "Yes":
+                  accountObj.deposit(amt);
+                  print("\nUpdated Balance: " + str(accountObj.getBalance()) + " n")
+               else:
+                   break
+ 
+           elif selection == 4:
+               print("nTransaction is now complete.")
+               print("Transaction number: ", random.randint(10000, 1000000))
+               print("Current Interest Rate: ", accountObj.annualInterestRate)
+               print("Monthly Interest Rate: ", accountObj.annualInterestRate / 12)
+               print("Thanks for choosing us as your bank")
+               exit()
+ 
+            else:
+               print("nThat's an invalid choice.")
+ 
+main()
